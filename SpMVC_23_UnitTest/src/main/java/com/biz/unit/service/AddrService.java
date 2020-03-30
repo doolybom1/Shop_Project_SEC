2f9419b60920702165922ad478a2584b9f859313
doolybom1 <elqkqkwkd@gmail.com>
2020-03-30 16:38:30 +0900

    Junit 테스트

---
+++ SpMVC_23_UnitTest/src/main/java/com/biz/unit/service/AddrService.java
@@ -0,0 +1,23 @@
+package com.biz.unit.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddrService {
+
+	public Map<String, String> getAddr() {
+		Map<String, String> addr = new HashMap<String, String>();
+		addr.put("name", "홍길동");
+		addr.put("addr", "광주광역시");
+		addr.put("tel", "010-111-1111");
+
+		return addr;
+	}
+
+	public String getName(String name) {
+		return "내이름은" + name + "입니다";
+	}
+}
